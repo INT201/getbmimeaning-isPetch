985957b6d470391506792805503c1e45bc3b0a94
isPetch <88103945+isPetch@users.noreply.github.com>
2022-09-10 21:07:37 +0700

    Update getBMIMeaning.js

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -1,9 +1,20 @@
 const { template } = require('@babel/core')
 
 function calculateBMI(weight, height) {
-  //code here
+  return weight/( (height/100) *  (height/100))
 }
 function getBMIMeaning(weight, height) {
-  //code here
+  let BMI = calculateBMI(weight, height)
+  if(BMI < 18.5){
+    return  "Underweight"
+  }
+  if(BMI > 25.0){
+    return "Overweight"
+  }
+  else
+  return "Normal weight"
 }
 module.exports = getBMIMeaning
+console.log(getBMIMeaning(30,160))
+console.log(getBMIMeaning(50,160))
+console.log(getBMIMeaning(100,160))
